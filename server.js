07acfa9b59ee2bcbb10d6ec7cf2f66b80fb5040f
John Murphy <chiefsmurph@gmail.com>
2016-06-08 13:53:46 -0700

    send flash

--- server.js
+++ server.js
@@ -6,5 +6,18 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 5000; // Use the port that Heroku
 server.listen(port);
 
+console.log('listening on ' + port);
 
 app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function (socket) {
+  console.log('connected socket');
+});
+
+function sendFlash() {
+  io.sockets.emit('flash');
+  console.log('flash')
+  setTimeout(sendFlash, (10 + Math.random() * 10) * 1000 );
+}
+
+setTimeout(sendFlash, 2000);
