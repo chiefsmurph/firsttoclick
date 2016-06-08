a4383c14f52537b508d49de1442613619408b1ee
John Murphy <chiefsmurph@gmail.com>
2016-06-08 13:11:55 -0700

    initial

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 5000; // Use the port that Heroku
+server.listen(port);
+
+
+app.use(express.static(__dirname + '/public'));
